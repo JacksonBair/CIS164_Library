c2c7e371f565034c52344973b4214bb55e98f99b
JacksonB <jacksonbair@gmail.com>
2024-09-30 13:19:41 -0500

    gitignore Cmake Update. basic Cmake structure

---
+++ CIS164_Library/CIS164_Library/CIS164_Library.cpp
@@ -0,0 +1,12 @@
+﻿// CIS164_Library.cpp : Defines the entry point for the application.
+//
+
+#include "CIS164_Library.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CIS164_Library/CIS164_Library/CIS164_Library.h
@@ -0,0 +1,8 @@
+﻿// CIS164_Library.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
